663d65e062f78ecd93ef30b9d0074253a6f25f4b
Justin Jean <JMJEAN@student.fullsail.edu>
2025-04-25 17:25:54 -0400

    Error message for API handler.

--- app/index.js
+++ app/index.js
@@ -6,8 +6,14 @@ const routeHandler = require('./routes');
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.get ('/', (req, res) => {   
+app.get ('/', (req, res) => {  
+    console.log('API is running'); 
 res.status(200).json({ message: "API is running", succes: true});
+});
+
+app.get ('/', (req, res) => {
+    console.error('Error: API not found');
+    res.status(404).json({ message: "API not found", success: false });  
 });
 
 app.use('/api/v1', routeHandler);
