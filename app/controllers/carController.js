de7d51e77fe72db7f4d18240d951252c408f86af
Justin Jean <JMJEAN@student.fullsail.edu>
2025-04-25 17:14:44 -0400

    Finished 2.6 - NodeJS MongoDB Assignment

---
+++ app/controllers/carController.js
@@ -0,0 +1,129 @@
+const Cars = require('../models/cars')
+
+cars = [{
+    "_id": "680bd3c3cfc35f2daf9fb20d",
+    "id": 1,
+    "make": "Nissan",
+    "model": "Skyline",
+    "year": 1998,
+    "horsepower": "340hp",
+    "completed": "true",
+    "__v": 0
+},
+{
+    "_id": "680bd4564fb1df97ef45d65d",
+    "id": 2,
+    "make": "Toyota",
+    "model": "Supra",
+    "year": 1997,
+    "horsepower": "300hp",
+    "completed": "true",
+    "__v": 0
+},
+{
+    "_id": "680bd4664fb1df97ef45d65f",
+    "id": 3,
+    "make": "Mazda",
+    "model": "RX-7",
+    "year": 2000,
+    "horsepower": "310hp",
+    "completed": "true",
+    "__v": 0
+},
+{
+    "_id": "680bd4704fb1df97ef45d661",
+    "id": 4,
+    "make": "Audi",
+    "model": "R-8",
+    "year": 2008,
+    "horsepower": "320hp",
+    "completed": "true",
+    "__v": 0
+},
+{
+    "_id": "680bdabe791ddd240e4ef11b",
+    "id": 5,
+    "make": "Tesla",
+    "model": "Model Y",
+    "year": 2024,
+    "horsepower": "350hp",
+    "completed": "true",
+    "__v": 0
+}]
+
+//get all cars
+const getAllCars = async (req, res) => {
+    const cars = await Cars.find({});
+    console.log('GET ALL cars')
+        res.status(200).json({ message: 'Get ALL successful', data: cars, success: true});
+    };
+
+//get car by id
+const getCarById = async (req, res) => {
+    const id = (req.params.id);
+    const car = await Cars.findById(id);
+    console.log('ID is:', id);
+
+    res.status(200).json({ 
+    message: 'Get by Id successful', 
+    id: id, 
+    data: car, 
+    success: true
+    });
+};
+
+//post a new car
+const createCar = async (req, res) => {
+    const newCar= await Cars.create(req.body, { new: true });
+    console.log('New car is:', newCar);
+    
+    if (!newCar || !newCar.make || !newCar.model || !newCar.year || !newCar.horsepower) {
+        return res.status(400).json({ 
+            message: 'Invalid car data', 
+            success: false, 
+            data: newCar
+        });
+    }
+    
+    res.status(201).json({ 
+        message: 'Create successful', 
+        data: newCar, 
+        success: true
+    });
+};
+
+//update car by id
+const updateCarById = async (req, res) => {
+    const id = (req.params.id);
+    const car = await Cars.findByIdAndUpdate(id, req.body, { new: true });
+    console.log('ID is:', id);
+    
+    res.status(200).json({ 
+        message: 'Update successful', 
+        id: id, 
+        data: car, 
+        success: true
+    });
+    };
+
+//delete car by id
+const deleteCarById = async (req, res) => {
+    const {id} = (req.params.id);
+    const car = await Cars.findByIdAndDelete(id, req.body, { new: true });
+    console.log('ID is:', id);
+    
+    res.status(200).json({ 
+        message: 'Delete successful', 
+        id: id, 
+        data: cars, 
+        success: true
+    });
+    };
+
+    module.exports = {
+        getAllCars,
+        getCarById,
+        createCar,
+        updateCarById,
+        deleteCarById
+    };
---
+++ app/controllers/horsepowerController.js
@@ -0,0 +1,129 @@
+const Horsepowers = require('../models/horsepowers')
+
+horsepowers = [{
+    "_id": "680bd5eaec979149c79149bb",
+    "id": 1,
+    "make": "Nissan",
+    "model": "Skyline",
+    "year": 1998,
+    "horsepower": "340hp",
+    "completed": "true",
+    "__v": 0
+},
+{
+    "_id": "680bd614ec979149c79149bd",
+    "id": 2,
+    "make": "Toyota",
+    "model": "Supra",
+    "year": 1997,
+    "horsepower": "300hp",
+    "completed": "true",
+    "__v": 0
+},
+{
+    "_id": "680bd620ec979149c79149bf",
+    "id": 3,
+    "make": "Mazda",
+    "model": "RX-7",
+    "year": 2000,
+    "horsepower": "310hp",
+    "completed": "true",
+    "__v": 0
+},
+{
+    "_id": "680bd62fec979149c79149c1",
+    "id": 4,
+    "make": "Audi",
+    "model": "R-8",
+    "year": 2008,
+    "horsepower": "320hp",
+    "completed": "true",
+    "__v": 0
+},
+{
+    "_id": "680bdac5791ddd240e4ef11d",
+    "id": 5,
+    "make": "Tesla",
+    "model": "Model Y",
+    "year": 2024,
+    "horsepower": "350hp",
+    "completed": "true",
+    "__v": 0
+}]
+
+//get all cars
+const getAllHorsepowers = async (req, res) => {
+    const horsepowers = await Horsepowers.find({});
+    console.log('GET ALL cars')
+        res.status(200).json({ message: 'Get ALL successful', data: horsepowers, success: true});
+    };
+
+//get car by id
+const getHorsepowerById = async (req, res) => {
+    const id = (req.params.id);
+    const horsepower = await Horsepowers.findById(id);
+    console.log('ID is:', id);
+
+    res.status(200).json({ 
+    message: 'Get by Id successful', 
+    id: id, 
+    data: horsepower, 
+    success: true
+    });
+};
+
+//post a new car
+const createHorsepower = async (req, res) => {
+    const newHorsepower= await Horsepowers.create(req.body, { new: true });
+    console.log('New car is:', newHorsepower);
+    
+    if (!newHorsepower || !newHorsepower.make || !newHorsepower.model || !newHorsepower.year || !newHorsepower.horsepower) {
+        return res.status(400).json({ 
+            message: 'Invalid car data', 
+            success: false, 
+            data: newHorsepower
+        });
+    }
+
+    res.status(201).json({ 
+        message: 'Create successful', 
+        success: true,
+        data: newHorsepower, 
+    });
+};
+
+//update car by id
+const updateHorsepowerById = async (req, res) => {
+    const id = (req.params.id);
+    const horsepower = await Horsepowers.findByIdAndUpdate(id, req.body,{ new: true });
+    console.log('ID is:', id);
+    
+    res.status(200).json({ 
+        message: 'Update successful', 
+        id: id, 
+        data: horsepower, 
+        success: true
+    });
+    };
+
+//delete car by id
+const deleteHorsepowerById = async (req, res) => {
+    const {id} = (req.params.id);
+    const horsepower = await Horsepowers.findByIdAndDelete(id, req.body, { new: true });
+    console.log('ID is:', id);
+
+    res.status(200).json({ 
+        message: 'Delete successful', 
+        id: id, 
+        data: horsepowers, 
+        success: true
+    });
+    };
+
+    module.exports = {
+        getAllHorsepowers,
+        getHorsepowerById,
+        createHorsepower,
+        updateHorsepowerById,
+        deleteHorsepowerById
+    };
