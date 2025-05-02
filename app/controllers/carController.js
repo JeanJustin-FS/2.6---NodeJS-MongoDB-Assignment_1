0a080ac60c414187a7dc813b88ff19bc0a8f98ae
Justin Jean <JMJEAN@student.fullsail.edu>
2025-05-02 03:39:19 -0400

    final

--- app/controllers/carController.js
+++ app/controllers/carController.js
@@ -1,115 +1,112 @@
-const Cars = require('../models/cars')
-const Car = require('../models/horsepowers')
+const Cars = require("../models/cars");
+const Car = require("../models/horsepowers");
 
-cars = []
+cars = [];
 
 //get all cars
 const getAllCars = async (req, res) => {
-    let queryString = JSON.stringify(req.query);
-    queryString = queryString.replace(/\b(gt|lt|gte|lte)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryString));
-    const cars = await Cars.find(JSON.parse(queryString));
-    try{
-    res.status(200).json ({
-        message: 'Get all successful',
-        data: cars,
-        success: true
+  let queryString = JSON.stringify(req.query);
+  queryString = queryString.replace(/\b(gt|lt|gte|lte)\b/g, match => `$${match}`);
+  console.log(JSON.parse(queryString));
+  const cars = await Cars.find(JSON.parse(queryString));
+  try {
+    res.status(200).json({
+      message: "Get all successful",
+      data: cars,
+      success: true,
     });
-    } catch (error) {
-        res.status(500).json ({
-        message: 'Error getting all cars',
-        success: false,
-        data: error
-        });
-    }  
+  } catch (error) {
+    res.status(500).json({
+      message: "Error getting all cars",
+      success: false,
+      data: error,
+    });
+  }
 };
 
 //get car by id
 const getCarById = async (req, res) => {
-    const { id } = (req.params);
-    const car = await Cars.findById(id, req.body,{new: true}); 
-    console.log('ID is:', id);
-    try {
-    res.status(200).json ({
-        message: 'Get by ID successful',
-        data: car,
-        success: true
+  const { id } = req.params;
+  const car = await Cars.findById(id, req.body, { new: true });
+  console.log("ID is:", id);
+  try {
+    res.status(200).json({
+      message: "Get by ID successful",
+      data: car,
+      success: true,
     });
-    } catch (error) {
-        res.status(404).json ({
-            message: 'Car not found',
-            success: false,
-            data: car
-        });
-    }
+  } catch (error) {
+    res.status(404).json({
+      message: "Car not found",
+      success: false,
+      data: car,
+    });
+  }
 };
 
 //post a new car
 const createCar = async (req, res) => {
-    try {    
+  try {
     const car = await Car.create(req.body);
-    console.log('Request body:', req.body);
-    console.log('Car created:', car);
-    
+
     res.status(201).json({
-        success: true,
-        message: 'Car created successfully',
-        data: car
+      success: true,
+      message: "Car created successfully",
+      data: car,
     });
-    
-    } catch (error) {
-    console.error('Error creating car:', error);
+  } catch (error) {
+    console.error("Error creating car:", error);
     res.status(400).json({
-        success: false,
-        message: error.message || 'Failed to create car'
+      success: false,
+      message: error.message || "Failed to create car",
     });
-    }
+  }
 };
 
 //update car by id
 const updateCarById = async (req, res) => {
-        const id = (req.params.id);
-        const car = await Cars.findByIdAndUpdate(id, req.body,{ new: true });
-        console.log('ID is:', id);
-        try{
-        res.status(200).json ({
-            message: 'Update successful',
-            data: car,
-            success: true
-        });
-        } catch (error) {
-            res.status(404).json ({
-                message: 'Car not found',
-                success: false,
-                data: car
-            });
-        }
-    };
+  const id = req.params.id;
+  const car = await Cars.findByIdAndUpdate(id, req.body, { new: true });
+  console.log("ID is:", id);
+  try {
+    res.status(200).json({
+      message: "Update successful",
+      data: car,
+      success: true,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: "Car not found",
+      success: false,
+      data: car,
+    });
+  }
+};
 
 //delete car by id
 const deleteCarById = async (req, res) => {
-    const id = (req.params.id);
-    const car = await Cars.findByIdAndDelete(id, req.body, { new: true });
-    console.log('ID is:', id);  
-    try {
-        res.status(200).json ({
-            message: 'Delete successful',
-            data: car,
-            success: true
-        });
-    } catch (error) {
-        res.status(404).json ({
-            message: 'Car not found',
-            success: false,
-            data: car
-        });
-    }
+  const id = req.params.id;
+  const car = await Cars.findByIdAndDelete(id, req.body, { new: true });
+  console.log("ID is:", id);
+  try {
+    res.status(200).json({
+      message: "Delete successful",
+      data: car,
+      success: true,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: "Car not found",
+      success: false,
+      data: car,
+    });
+  }
 };
 
-    module.exports = {
-        getAllCars,
-        getCarById,
-        createCar,
-        updateCarById,
-        deleteCarById
-    };
+module.exports = {
+  getAllCars,
+  getCarById,
+  createCar,
+  updateCarById,
+  deleteCarById,
+};
--- app/controllers/horsepowerController.js
+++ app/controllers/horsepowerController.js
@@ -1,118 +1,115 @@
-const Horsepowers = require('../models/horsepowers')
-const Horsepower = require('../models/cars')
+const Horsepowers = require("../models/horsepowers");
+const Horsepower = require("../models/cars");
 
-horsepowers = []
+horsepowers = [];
 
 //get all cars
 const getAllHorsepowers = async (req, res) => {
-    let queryString = JSON.stringify(req.query);
-    queryString = queryString.replace(/\b(gt|lt|gte|lte)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryString));
-    const horsepowers = await Horsepowers.find(JSON.parse(queryString));
-    try{
-    res.status(200).json ({
-        message: 'Get all successful',
-        data: horsepowers,
-        success: true
+  let queryString = JSON.stringify(req.query);
+  queryString = queryString.replace(/\b(gt|lt|gte|lte)\b/g, match => `$${match}`);
+  console.log(JSON.parse(queryString));
+  const horsepowers = await Horsepowers.find(JSON.parse(queryString));
+  try {
+    res.status(200).json({
+      message: "Get all successful",
+      data: horsepowers,
+      success: true,
     });
-    } catch (error) {
-    res.status(500).json ({
-        message: 'Error getting all horsepowers',
-        success: false,
-        data: error
+  } catch (error) {
+    res.status(500).json({
+      message: "Error getting all horsepowers",
+      success: false,
+      data: error,
     });
-    }
+  }
 };
 
 //get horsepower by id
 const getHorsepowerById = async (req, res) => {
-    const { id } = (req.params);
-    const horsepower = await Horsepowers.findById(id , req.body,{ new: true });
-    console.log('ID is:', id);
+  const { id } = req.params;
+  const horsepower = await Horsepowers.findById(id, req.body, { new: true });
+  console.log("ID is:", id);
 
-    try {
-    res.status(200).json ({
-        message: 'Get by ID successful',
-        data: horsepower,
-        success: true
+  try {
+    res.status(200).json({
+      message: "Get by ID successful",
+      data: horsepower,
+      success: true,
     });
-    } catch (error) {
-        res.status(404).json ({
-            message: 'Horsepower not found',
-            success: false,
-            data: horsepower
-        });
-    }
+  } catch (error) {
+    res.status(404).json({
+      message: "Horsepower not found",
+      success: false,
+      data: horsepower,
+    });
+  }
 };
 
 //post a new car
 const createHorsepower = async (req, res) => {
-    try {    
+  try {
     const horsepower = await Horsepower.create(req.body);
-    console.log('Request body:', req.body);
-    console.log('Horsepower created:', horsepower);
 
     res.status(201).json({
-        success: true,
-        message: 'Horsepower created successfully',
-        data: horsepower
+      success: true,
+      message: "Horsepower created successfully",
+      data: horsepower,
     });
-    
-    } catch (error) {
-    console.error('Error creating horsepower:', error);
+  } catch (error) {
+    console.error("Error creating horsepower:", error);
     res.status(400).json({
-        success: false,
-        message: error.message || 'Failed to create horsepower'
+      success: false,
+      message: error.message || "Failed to create horsepower",
     });
-    }
+  }
 };
 
 //update car by id
 const updateHorsepowerById = async (req, res) => {
-    const id = (req.params.id);
-    const horsepower = await Horsepowers.findByIdAndUpdate(id, req.body,{ new: true });
-    console.log('ID is:', id);
-    try{
-    res.status(200).json ({
-        message: 'Update successful',
-        id: id,
-        data: horsepower,
-        success: true
+  const id = req.params.id;
+  const horsepower = await Horsepowers.findByIdAndUpdate(id, req.body, { new: true });
+  console.log("ID is:", id);
+  try {
+    res.status(200).json({
+      message: "Update successful",
+      id: id,
+      data: horsepower,
+      success: true,
     });
-    } catch (error) {
-        res.status(404).json ({
-            message: 'Horsepower not found',
-            success: false,
-            data: horsepower
-        });
-    }
-    };
+  } catch (error) {
+    res.status(404).json({
+      message: "Horsepower not found",
+      success: false,
+      data: horsepower,
+    });
+  }
+};
 
 //delete car by id
 const deleteHorsepowerById = async (req, res) => {
-    const id = (req.params.id);
-    const horsepower = await Horsepowers.findByIdAndDelete(id, req.body, { new: true });
-    console.log('ID is:', id);
-    try {
-        res.status(200).json ({
-            message: 'Delete successful',
-            id: id,
-            data: horsepower,
-            success: true
-        });
-    } catch (error) {
-        res.status(404).json ({
-            message: 'Horsepower not found',
-            success: false,
-            data: horsepower
-        });
-    }
-    };
+  const id = req.params.id;
+  const horsepower = await Horsepowers.findByIdAndDelete(id, req.body, { new: true });
+  console.log("ID is:", id);
+  try {
+    res.status(200).json({
+      message: "Delete successful",
+      id: id,
+      data: horsepower,
+      success: true,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: "Horsepower not found",
+      success: false,
+      data: horsepower,
+    });
+  }
+};
 
-    module.exports = {
-        getAllHorsepowers,
-        getHorsepowerById,
-        createHorsepower,
-        updateHorsepowerById,
-        deleteHorsepowerById
-    };
+module.exports = {
+  getAllHorsepowers,
+  getHorsepowerById,
+  createHorsepower,
+  updateHorsepowerById,
+  deleteHorsepowerById,
+};
